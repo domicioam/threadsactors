4bd3990ecd2e4ae2eeb723d2d7a22ef68484c880
Domício Medeiros <domicioam@gmail.com>
2021-02-11 18:21:48 +0000

    Doing infinite loop in the actor manner.

--- ThreadsActors/Actors/ActorMain.cs
+++ ThreadsActors/Actors/ActorMain.cs
@@ -21,46 +21,58 @@ namespace ThreadsActors.Actors
             });
 
             Receive<Execute>((message) => Execute());
+            Receive<RunThreads>(message => RunThreads());
+        }
+
+        private void RunThreads()
+        {
+            this.counterActor = Context.ActorOf<CounterActor>();
+
+            Thread t1 = new Thread(new ThreadStart(DoWorkAsync));
+            Thread t2 = new Thread(new ThreadStart(DoWorkAsync));
+            Thread t3 = new Thread(new ThreadStart(DoWorkAsync));
+            Thread t4 = new Thread(new ThreadStart(DoWorkAsync));
+            Thread t5 = new Thread(new ThreadStart(DoWorkAsync));
+            Thread t6 = new Thread(new ThreadStart(DoWorkAsync));
+            Thread t7 = new Thread(new ThreadStart(DoWorkAsync));
+            Thread t8 = new Thread(new ThreadStart(DoWorkAsync));
+            Thread t9 = new Thread(new ThreadStart(DoWorkAsync));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+            t6.Start();
+            t7.Start();
+            t8.Start();
+            t9.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+            t4.Join();
+            t5.Join();
+            t6.Join();
+            t7.Join();
+            t8.Join();
+            t9.Join();
+
+            this.counterActor.Tell(new GetValue(Self));
         }
 
         public void Execute()
         {
-            while (true) // can't have infinte loop, will prevent receiving future messages
-            {
-                this.counterActor = Context.ActorOf<CounterActor>();
-
-                Thread t1 = new Thread(new ThreadStart(DoWorkAsync));
-                Thread t2 = new Thread(new ThreadStart(DoWorkAsync));
-                Thread t3 = new Thread(new ThreadStart(DoWorkAsync));
-                Thread t4 = new Thread(new ThreadStart(DoWorkAsync));
-                Thread t5 = new Thread(new ThreadStart(DoWorkAsync));
-                Thread t6 = new Thread(new ThreadStart(DoWorkAsync));
-                Thread t7 = new Thread(new ThreadStart(DoWorkAsync));
-                Thread t8 = new Thread(new ThreadStart(DoWorkAsync));
-                Thread t9 = new Thread(new ThreadStart(DoWorkAsync));
-
-                t1.Start();
-                t2.Start();
-                t3.Start();
-                t4.Start();
-                t5.Start();
-                t6.Start();
-                t7.Start();
-                t8.Start();
-                t9.Start();
-
-                t1.Join();
-                t2.Join();
-                t3.Join();
-                t4.Join();
-                t5.Join();
-                t6.Join();
-                t7.Join();
-                t8.Join();
-                t9.Join();
-
-                this.counterActor.Tell(new GetValue(Self));
-            }
+            Context
+                .System
+                .Scheduler
+                .ScheduleTellRepeatedly(
+                    TimeSpan.FromSeconds(0),
+                    TimeSpan.FromSeconds(0.1),
+                    Self,
+                    new RunThreads(),
+                    ActorRefs.NoSender
+                );
         }
 
         private void DoWorkAsync()
---
+++ ThreadsActors/Actors/Messages/RunThreads.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadsActors.Actors.Messages
+{
+    class RunThreads
+    {
+    }
+}
